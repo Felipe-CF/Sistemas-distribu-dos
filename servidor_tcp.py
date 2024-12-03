b6faa222f497c62c4efaf74da058dd74e5a2db7e
Felipe-CF <prog.study.fcf@gmail.com>
2024-12-03 13:35:35 -0300

    criação de uma comunicação udp passando uma lista

--- servidor_tcp.py
+++ servidor_tcp.py
@@ -15,22 +15,31 @@ tcp.bind(MEU_SERVIDOR)  # Faz o bind do IP e da porta para começar a ouvir
 
 tcp.listen(1)  # Começar a ouvir (aguardar conexão)
 
-print(f"Servidor ouvindo na porta {MINHA_PORTA}...")
+# print(f"Servidor ouvindo na porta {MINHA_PORTA}...")
 
 # Aceitar conexão do cliente
 conexao, docliente = tcp.accept()
-print("O cliente =", docliente, "se conectou")
+
+# print("O cliente =", docliente, "se conectou")
+
+matriz_de_lotes = Matriz.maatriz()
 
 teste = True
 
+print("| -----  Lote Premiado começou!   ----- |")
 # Loop para receber mensagens do cliente
 while teste:
+    
     Mensagem_Recebida = conexao.recv(1024)
 
     if Mensagem_Recebida:
+
+        mensagem = Mensagem_Recebida.decode('utf8')
+
+        if mensagem == 'matriz':
         # Se houver uma nova mensagem, imprime na tela
-        print("Recebi =", Mensagem_Recebida.decode("utf8"), ", Do cliente", docliente)
-        teste = False
+            print("Recebi =", Mensagem_Recebida.decode("utf8"), ", Do cliente", docliente)
+            teste = False
 
 # Fechar a conexão ao terminar
 conexao.close()
