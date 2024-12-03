b9c8af1eb8f31eaf4078d6be96fdb569940d3775
Giovana Beatriz Barros Firmino da Silva <20231014040030@ifrn.local>
2024-12-03 11:45:47 -0300

    Adição de .md das comunicações TCP e UDP

---
+++ servidor.py
@@ -0,0 +1,36 @@
+import socket
+from matriz import Matriz
+
+MINHA_PORTA = 8000
+
+MEU_IP = '127.0.0.1'
+
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Definir o IP e porta para o servidor ouvir
+MEU_SERVIDOR = (MEU_IP, MINHA_PORTA)
+
+tcp.bind(MEU_SERVIDOR)  # Faz o bind do IP e da porta para começar a ouvir
+
+tcp.listen(1)  # Começar a ouvir (aguardar conexão)
+
+print(f"Servidor ouvindo na porta {MINHA_PORTA}...")
+
+# Aceitar conexão do cliente
+conexao, docliente = tcp.accept()
+print("O cliente =", docliente, "se conectou")
+
+teste = True
+
+# Loop para receber mensagens do cliente
+while teste:
+    Mensagem_Recebida = conexao.recv(1024)
+
+    if Mensagem_Recebida:
+        # Se houver uma nova mensagem, imprime na tela
+        print("Recebi =", Mensagem_Recebida.decode("utf8"), ", Do cliente", docliente)
+        teste = False
+
+# Fechar a conexão ao terminar
+conexao.close()
